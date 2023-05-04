256b8148e0174c80c9434579bb233cad23044705
OmairaBarrera <lomaira.barrera@gmail.com>
2023-05-04 06:54:40 -0500

    refactor: :art: component organization
    Moved components to their respective folders.

--- Actividad-1/main.js
+++ Actividad-1/main.js
@@ -1,5 +1,5 @@
-import "./components/my-header.js";
-import "./components/my-section.js"
-import "./components/my-seleccion.js"
-import "./components/my-nav.js"
-import "./components/my-footer.js"
+import "./components/my-header/my-header.js";
+import "./components/my-section/my-section.js"
+import "./components/my-seleccion/my-seleccion.js"
+import "./components/my-nav/my-nav.js"
+import "./components/my-footer/my-footer.js"
